c70c38209a4aeededbb48c4e0f54265b959da211
plostvik <plostvik@gmail.com>
2023-07-28 19:52:18 +0300

    UC#14 Implement i18n

--- src/components/NumberFormatter.jsx
+++ src/components/NumberFormatter.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import style from './numberFormatter.module.css';
+
 const NumberFormatter = () => {
   const [numberFormatter, setNumberFormatter] = useState('1');
   const { t } = useTranslation();
@@ -10,52 +12,56 @@ const NumberFormatter = () => {
   };
 
   return (
-    <>
-      <h2>{t('digits')}</h2>
+    <div className={style.wrapper}>
       <div>
-        {formatterOptions.map(option => {
-          return (
-            <div key={option}>
-              <label htmlFor={option}>{option}</label>
-              <input
-                type="radio"
-                value={option}
-                id={option}
-                checked={numberFormatter === option}
-                onChange={onOptionChange}
-              />
-            </div>
-          );
-        })}
+        <h2>{t('digits')}</h2>
+        <ul>
+          {formatterOptions.map(option => {
+            return (
+              <li key={option}>
+                <label htmlFor={option}>{option}</label>
+                <input
+                  type="radio"
+                  value={option}
+                  id={option}
+                  checked={numberFormatter === option}
+                  onChange={onOptionChange}
+                />
+              </li>
+            );
+          })}
+        </ul>
       </div>
-      <h4>{t('output')}</h4>
-      <ul>
-        <li>
-          {t('number', {
-            value: 123_456_789.22,
-            minimumFractionDigits: numberFormatter,
-          })}
-        </li>
-        <li>
-          {t('number', {
-            value: 150_000_00,
-            minimumFractionDigits: numberFormatter,
-          })}
-        </li>
-        <li>
-          {t('number', {
-            value: 1_408.23,
-            minimumFractionDigits: numberFormatter,
-          })}
-        </li>
-        <li>
-          {t('number', {
-            value: 99.2345,
-            minimumFractionDigits: numberFormatter,
-          })}
-        </li>
-      </ul>
-    </>
+      <div>
+        <h2>{t('output')}</h2>
+        <ul>
+          <li>
+            {t('number', {
+              value: 123_456_789.22,
+              minimumFractionDigits: numberFormatter,
+            })}
+          </li>
+          <li>
+            {t('number', {
+              value: 150_000_00,
+              minimumFractionDigits: numberFormatter,
+            })}
+          </li>
+          <li>
+            {t('number', {
+              value: 1_408.23,
+              minimumFractionDigits: numberFormatter,
+            })}
+          </li>
+          <li>
+            {t('number', {
+              value: 99.2345,
+              minimumFractionDigits: numberFormatter,
+            })}
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
