ec4b36dccaf351cce99cab065eb4e36cf4ed5e81
plostvik <plostvik@gmail.com>
2023-08-02 12:06:22 +0300

    update locale for ua date

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -1,15 +1,16 @@
 import { useTranslation } from 'react-i18next';
 
 const DateTime = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const locale = i18n.language === 'ua' ? 'uk-UA' : i18n.language;
 
   return (
     <>
       <h2>{t('date.header')}</h2>
       <ul>
         <li>{t('date.standard', { value: Date.now() })}</li>
-        <li>{t('date.full', { value: Date.now() })}</li>
-        <li>{t('date.short', { value: Date.now() })}</li>
+        <li>{t('date.full', { value: Date.now(), lng: locale })}</li>
+        <li>{t('date.short', { value: Date.now(), lng: locale })}</li>
         <li>{t('date.time', { value: Date.now() })}</li>
       </ul>
     </>
